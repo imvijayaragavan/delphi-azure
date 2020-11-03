d26b318ff33c5045256f24baf966725d094355db
imvijayaragavan <vijay.srinivasan@darkmatter.ae>
2020-11-03 20:35:26 +0400

    Updated the obsolute

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -2,5 +2,5 @@ resource "azurerm_subnet" "current" {
   name                      = var.subnet_name
   resource_group_name       = var.resource_group_name
   virtual_network_name      = var.vnet_name
-  address_prefix            = var.subnet_prefix 
+  address_prefixes          = var.subnet_prefix 
 }
